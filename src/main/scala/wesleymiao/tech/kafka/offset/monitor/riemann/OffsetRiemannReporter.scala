2434223a0ac72905d432ad233380c786685594a7
Ubuntu <azureuser@fw-cb-prd5.z11j0kyn5gsetah25x4jqazbrb.bx.internal.cloudapp.net>
2016-05-06 09:42:57 +0000

    aggregate by topic+group

--- src/main/scala/wesleymiao/tech/kafka/offset/monitor/riemann/OffsetRiemannReporter.scala
+++ src/main/scala/wesleymiao/tech/kafka/offset/monitor/riemann/OffsetRiemannReporter.scala
@@ -10,16 +10,19 @@ class OffsetRiemannReporter (pluginsArgs: String) extends com.quantifind.kafka.o
   override def report(info: scala.IndexedSeq[OffsetInfo]) =  {
     // translate to Riemann Event format
     type RIEMANNEVENT = (String /*host*/, String /*service*/, Option[Long] /*metric*/, Seq[String] /*tags*/, Option[String] /*state*/, Option[String] /*description*/)
-    val events = info.flatMap { i => Seq[RIEMANNEVENT](
-      (getMetricName(i), "logSize", Some(i.logSize), Seq(), Some("ok"), None),
-      (getMetricName(i), "offset", Some(i.offset), Seq(), Some("ok"), None),
-      (getMetricName(i), "lag", Some(i.lag), Seq(), if (i.lag > 0) Some("critical") else Some("ok"), None)
+    val grouped = info.groupBy(i => (i.topic, i.group))
+    val groupedReduced = grouped.mapValues (events => events.map(i => (i.logSize, i.offset, i.lag)).reduce( (a, b) => (a._1 + b._1, a._2 + b._2, a._3 + b._3)))
+    val events = groupedReduced.flatMap {
+      case ((topic, group), (logSize, offset, lag)) => Seq[RIEMANNEVENT](
+      (topic+"_"+group, "logSize", Some(logSize), Seq(), Some("ok"), None),
+      (topic+"_"+group, "offset", Some(offset), Seq(), Some("ok"), None),
+      (topic+"_"+group, "lag", Some(lag), Seq(), if (lag > 10) Some("critical") else Some("ok"), None)
     )}
 
     RiemannConnection.send(RiemannReporterArguments.riemannHost, RiemannReporterArguments.riemannPort, events.toIterator)
   }
 
   def getMetricName(offsetInfo: OffsetInfo): String = {
-    offsetInfo.topic.replace(".", "_") + "." + offsetInfo.group.replace(".", "_") + "." + offsetInfo.partition
+    offsetInfo.topic.replace(".", "_") + "_" + offsetInfo.group.replace(".", "_") + "_" + offsetInfo.partition
   }
 }
