2d553fd4e2e4faae3f2105d33e02bc28474f9763
Ubuntu <azureuser@fw-cb-prd5.z11j0kyn5gsetah25x4jqazbrb.bx.internal.cloudapp.net>
2016-05-09 07:41:18 +0000

    Add env as tag for all Riemman events

--- src/main/scala/wesleymiao/tech/kafka/offset/monitor/riemann/OffsetRiemannReporter.scala
+++ src/main/scala/wesleymiao/tech/kafka/offset/monitor/riemann/OffsetRiemannReporter.scala
@@ -12,11 +12,12 @@ class OffsetRiemannReporter (pluginsArgs: String) extends com.quantifind.kafka.o
     type RIEMANNEVENT = (String /*host*/, String /*service*/, Option[Long] /*metric*/, Seq[String] /*tags*/, Option[String] /*state*/, Option[String] /*description*/)
     val grouped = info.groupBy(i => (i.topic, i.group))
     val groupedReduced = grouped.mapValues (events => events.map(i => (i.logSize, i.offset, i.lag)).reduce( (a, b) => (a._1 + b._1, a._2 + b._2, a._3 + b._3)))
+    val env = RiemannReporterArguments.env
     val events = groupedReduced.flatMap {
       case ((topic, group), (logSize, offset, lag)) => Seq[RIEMANNEVENT](
-      (topic+"_"+group, "logSize", Some(logSize), Seq(), Some("ok"), None),
-      (topic+"_"+group, "offset", Some(offset), Seq(), Some("ok"), None),
-      (topic+"_"+group, "lag", Some(lag), Seq(), if (lag > 10) Some("critical") else Some("ok"), None)
+      (topic+"_"+group, "logSize", Some(logSize), Seq(env), Some("ok"), None),
+      (topic+"_"+group, "offset", Some(offset), Seq(env), Some("ok"), None),
+      (topic+"_"+group, "lag", Some(lag), Seq(env), if (lag > 10) Some("critical") else Some("ok"), None)
     )}
 
     RiemannConnection.send(RiemannReporterArguments.riemannHost, RiemannReporterArguments.riemannPort, events.toIterator)
--- src/main/scala/wesleymiao/tech/kafka/offset/monitor/riemann/RiemannReporterArguments.scala
+++ src/main/scala/wesleymiao/tech/kafka/offset/monitor/riemann/RiemannReporterArguments.scala
@@ -8,9 +8,12 @@ object RiemannReporterArguments {
 
   var riemannPort : Int = 5555
 
+  var env: String = "prd"
+
   def parseArguments(args: String) = {
     val argsMap: Map[String, String] = args.split(",").map(_.split("=", 2)).filter(_.length > 1).map(arg => { arg(0) -> arg(1) }).toMap
     argsMap.get("riemannHost").foreach(riemannHost = _)
     argsMap.get("riemannPort").foreach(str => {riemannPort = parseInt(str)})
+    argsMap.get("env").foreach(env = _)
   }
 }
