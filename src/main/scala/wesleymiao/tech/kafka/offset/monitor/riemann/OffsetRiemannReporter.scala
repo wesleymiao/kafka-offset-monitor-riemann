4dab1d50f866e19ec3161c8061b5f809d88ddf42
Wesley Miao <wemia@microsoft.com>
2016-05-09 11:43:15 +0000

    fix some wierd NoSuchMethodFound exceptions while using Time and Duration comparisions

--- src/main/scala/wesleymiao/tech/kafka/offset/monitor/riemann/OffsetRiemannReporter.scala
+++ src/main/scala/wesleymiao/tech/kafka/offset/monitor/riemann/OffsetRiemannReporter.scala
@@ -15,12 +15,12 @@ class OffsetRiemannReporter (pluginsArgs: String) extends com.quantifind.kafka.o
 
       type RIEMANNEVENT = (String /*host*/, String /*service*/, Option[Long] /*metric*/, Seq[String] /*tags*/, Option[String] /*state*/, Option[String] /*description*/)
       val grouped = info.groupBy(i => (i.topic, i.group))
-      val groupedReduced = grouped.mapValues (events => events.map(i => (i.logSize, i.offset, i.lag, i.modified)).reduce( (a, b) => (a._1 + b._1, a._2 + b._2, a._3 + b._3, if (a._4 >= b._4) a._4 else b._4)))
+      val groupedReduced = grouped.mapValues (events => events.map(i => (i.logSize, i.offset, i.lag, i.modified)).reduce( (a, b) => (a._1 + b._1, a._2 + b._2, a._3 + b._3, if (a._4.inSeconds >= b._4.inSeconds) a._4 else b._4)))
       val env = RiemannReporterArguments.env
       val events = groupedReduced.flatMap {
         case ((topic, group), (logSize, offset, lag, mtime)) =>
-          val isActive = if ((Time.now - mtime).inNanoseconds > 1.day.inNanoseconds) "inactive" else "active"
-          val inactiveFor = (Time.now - mtime).toString()
+          val isActive = if ((Time.now - mtime).inSeconds > 1.day.inSeconds) "inactive" else "active"
+          val inactiveFor = (Time.now - mtime).inSeconds.toString()
           val tags = Seq(env, isActive, inactiveFor)
           Seq[RIEMANNEVENT]((topic+"_"+group, "logSize", Some(logSize), tags, Some("ok"), None),
             (topic+"_"+group, "offset", Some(offset), tags, Some("ok"), None),
